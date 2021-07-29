2cd905de444b66ac1aa53cd504cb2369aa7006f4
Lucas Perez <lucastperez@gmail.com>
2021-07-28 21:06:40 -0300

    Created Items table

---
+++ lib/rockelivery/item.ex
@@ -0,0 +1,26 @@
+defmodule Rockelivery.Item do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params ~w[category description price photo]a
+
+  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+
+  schema "items" do
+    field :category, Ecto.Enum, values: ~w[food drink desert]a
+    field :description, :string
+    field :price, :decimal
+    field :photo, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_number(:price, greater_than: 0)
+  end
+end
