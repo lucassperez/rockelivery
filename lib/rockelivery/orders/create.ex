c7f09e8a680f06ed093bbb78aa902ae89b7e349e
Lucas Perez <lucastperez@gmail.com>
2021-07-31 17:16:54 -0300

    Created Orders.ValidateAndMultiplyItems

--- lib/rockelivery/orders/create.ex
+++ lib/rockelivery/orders/create.ex
@@ -1,11 +1,16 @@
 defmodule Rockelivery.Orders.Create do
   import Ecto.Query
   alias Rockelivery.{Error, Item, Order, Repo}
+  alias Rockelivery.Orders.ValidateAndMultiplyItems
 
   def call(params) do
-    params
-    |> fetch_items()
-    |> handle_items(params)
+    with {:ok, items_list} <- fetch_items(params),
+         {:ok, %Order{} = order} <- handle_items(items_list, params)
+    do
+      {:ok, order}
+    else
+      {:error, reason} -> {:error, Error.build(:bad_request, reason)}
+    end
   end
 
   defp fetch_items(%{"items" => items_params}) do
@@ -15,52 +20,12 @@ defmodule Rockelivery.Orders.Create do
 
     query
     |> Repo.all()
-    |> validate_items(items_ids)
-    |> multiply_items_if_valid(items_params)
+    |> ValidateAndMultiplyItems.call(items_ids, items_params)
   end
 
-  defp validate_items(items, items_ids) do
-    items_map = Map.new(items, fn item -> {item.id, item} end)
-
-    items_ids
-    |> Enum.map(fn id -> {id, Map.get(items_map, id)} end)
-    |> Enum.any?(fn {_id, value} -> is_nil(value) end)
-    |> fn items_invalid? ->
-      if items_invalid? do
-        {:error, "Invalid item id"}
-      else
-        {:ok, items_map}
-      end
-    end.()
-  end
-
-  defp multiply_items_if_valid({:error, reason}, _items_params),
-    do: {:error, reason}
-
-  defp multiply_items_if_valid({:ok, items_map}, items_params) do
-    items_list =
-      items_params
-      |> Enum.reduce([], fn %{"id" => id, "quantity" => quantity}, acc ->
-        item = Map.get(items_map, id)
-        List.duplicate(item, quantity) ++ acc
-      end)
-
-    {:ok, items_list}
-  end
-
-  defp handle_items({:ok, items_list}, params) do
+  defp handle_items(items_list, params) do
     params
     |> Order.changeset(items_list)
     |> Repo.insert()
-    |> handle_insert()
-  end
-
-  defp handle_items({:error, reason}, _params),
-    do: handle_insert({:error, reason})
-
-  defp handle_insert({:ok, %Order{} = order}), do: {:ok, order}
-
-  defp handle_insert({:error, reason}) do
-    {:error, Error.build(:bad_request, reason)}
   end
 end
---
+++ lib/rockelivery/orders/validate_and_multiply_items.ex
@@ -0,0 +1,38 @@
+defmodule Rockelivery.Orders.ValidateAndMultiplyItems do
+  def call(items, items_ids, items_params) do
+    items_map = Map.new(items, fn item -> {item.id, item} end)
+
+    items_ids
+    |> Enum.map(fn id -> {id, Map.get(items_map, id)} end)
+    |> Enum.any?(fn {_id, value} -> is_nil(value) end)
+    |> fn items_invalid? ->
+      if items_invalid? do
+        {:error, "Invalid item id"}
+      else
+        {:ok, items_map}
+      end
+    end.()
+    |> multiply_items_if_valid(items_params)
+  end
+
+  defp multiply_items_if_valid({:error, reason}, _items_params),
+    do: {:error, reason}
+
+  defp multiply_items_if_valid({:ok, items_map}, items_params) do
+    items_list =
+      items_params
+      |> Enum.reduce_while([], fn %{"id" => id, "quantity" => quantity}, acc ->
+        if quantity > 0 do
+          item = Map.get(items_map, id)
+          {:cont, List.duplicate(item, quantity) ++ acc}
+        else
+          {:halt, {:error, "Item quantity has to be greater than zero"}}
+        end
+      end)
+
+    case items_list do
+      {:error, reason} -> {:error, reason}
+      _ -> {:ok, items_list}
+    end
+  end
+end
