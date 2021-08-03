74221f79778efd05c9dc62834de74471abbe4fd0
Lucas Perez <lucastperez@gmail.com>
2021-08-02 23:07:50 -0300

    Created ViaCep.Client

--- mix.exs
+++ mix.exs
@@ -53,7 +53,9 @@ defmodule Rockelivery.MixProject do
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:pbkdf2_elixir, "~> 1.4"},
       {:excoveralls, "~> 0.10", only: :test},
-      {:ex_machina, "~> 2.7.0", only: :test}
+      {:ex_machina, "~> 2.7.0", only: :test},
+      {:tesla, "~> 1.4"},
+      {:hackney, "~> 1.17"}
     ]
   end
 
